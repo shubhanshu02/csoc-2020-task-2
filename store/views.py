04b4b7274f176f52d050842c7e242cc38457f764
shubhanshu02 <shubhanshu.saxena2002@gmail.com>
2020-05-05 17:16:35 +0530

    Store App Complete

--- store/views.py
+++ store/views.py
@@ -7,48 +7,49 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from store.models import Book, BookCopy, UserRating
-# Create your views here.
 
 def index(request):
     return render(request, 'store/index.html')
 
 def bookDetailView(request, bid):
     template_name = 'store/book_detail.html'
-
+    
+    # List of available book id(s)
     avail = []
     for i in Book.objects.all():
         avail.append(i.id)
     
+    # If the id in the request is in the list,  
     if bid in avail:
-        books = Book.objects.filter(id = bid).first()
-        num = BookCopy.objects.filter(book = books).filter(status = True).count()
+        book = Book.objects.filter(id = bid).first()
+        available_copies = BookCopy.objects.filter(book = book).filter(status = True).count()
+    # Else return page not found
     else:
         raise Http404('Book does not exist')
 
-    avgratings = UserRating.objects.filter(book = books)
-    r = 0
-    if len(avgratings) != 0:
-        for i in avgratings:
-            r += i.rating
-        r /= len(avgratings)
-    else:
-        r = -1
-    books.rating = round(r, 2)
-    books.save()
-    if request.user.is_authenticated and avgratings.count() > 0:
-        avgratings = avgratings.filter(user_id = request.user.id).first()
-        if avgratings != None:
-            avgratings = avgratings.rating
-    else:
-        avgratings = None
-    context = {
-        'book': books,
-        'num_available': num,
-        'usrating': avgratings,
-    }
+    ## USER GIVEN RATING OF THE BOOK ##
 
+    # All rating objects for the book
+    totalRatings = UserRating.objects.filter(book = book)
+    # check if user is logged in
+    # and rating objects are present for the book
+    if request.user.is_authenticated and totalRatings.count() > 0:
+        myRating = totalRatings.filter(user_id = request.user.id).first()
+        # If user rating is found
+        if myRating != None:
+            givenRating = myRating.rating
+    # Else the rating for the book by the user is None
+    else:
+        givenRating = None
+
+
+    context = {
+        'book': book,
+        'num_available': available_copies,
+        'usrating': givenRating,
+    }
     return render(request, template_name, context=context)
+
 
 
 @csrf_exempt
@@ -56,6 +57,9 @@ def bookListView(request):
     template_name = 'store/book_list.html'
     get_data = request.GET
 
+    ## SELECTION OF BOOK OBJECTS i.e. SEARCH ##
+
+    # search is case insensitive
     books = Book.objects.all()
     if 'title' in get_data.keys():
         books = books.filter(title__icontains = get_data['title'])
@@ -67,24 +71,12 @@ def bookListView(request):
     context = {
         'books': books,
     }
-    for i in books:
-        x = UserRating.objects.filter(book = i)
-        r = 0
-        if len(x) != 0:
-            for i in x:
-                r += i.rating
-            r /= len(x)
-        else:
-            r = -1
-        i.rating = round(r, 2)
-        i.save()
-
     return render(request, template_name, context=context)
 
 @login_required
 def viewLoanedBooks(request):
     template_name = 'store/loaned_books.html'
-
+    # Search for Loaned Books by the user
     books = BookCopy.objects.filter(borrower = request.user)
     context = {
         'books': books,
@@ -94,118 +86,178 @@ def viewLoanedBooks(request):
 @csrf_exempt
 @login_required
 def loanBookView(request):
-    
     '''
     Check if an instance of the asked book is available.
     If yes, then set the message to 'success', otherwise 'failure'
     '''
-    book_id = request.POST['bid']
-    books = BookCopy.objects.filter(book_id = book_id, status = True)
-    if books.count() > 0:
-        msg = 'success' 
-        books = books.first()
-        books.status = False
-        books.borrower = request.user
-        books.borrow_date = datetime.date.today()
-        books.save()
+    if request.method == 'POST':
+        # Get the copies of the required book if it is available for loan
+        book_id = request.POST['bid']
+        books = BookCopy.objects.filter(book_id = book_id, status = True)
+
+        # If copy is available
+        if books.count() > 0:
+            msg = 'success' 
+            # Select the first copy
+            books = books.first()
+            # Set to unavailable
+            books.status = False
+            # User is the borrower
+            books.borrower = request.user
+            # Current date is borrow date
+            books.borrow_date = datetime.date.today()
+            # Save the model object
+            books.save()
+        else:
+            # The following two are possible:
+            #   1. Either no such book is found, or
+            #   2. no such book is available for loan
+            msg = 'failure'
+
+        # Return the message
+        response_data = {
+            'message': msg,
+        }
+        return JsonResponse(response_data)
+
+    # This page is not available for user to view and is just for data exchange
     else:
-        msg = 'failure'
-    response_data = {
-        'message': msg,
-    }
-    return JsonResponse(response_data)
-
-
+        raise Http404
 
 
 @csrf_exempt
 @login_required
 def returnBookView(request):
-    id = request.POST['bid']
-    book = BookCopy.objects.filter(id = id, status = False).first()
-    if (book is not None):
-        book.status = True
-        book.borrower = None
-        book.borrow_date = None
-        book.save()
-        msg = 'success'
+    # Books are returned through POST method 
+    if request.method == 'POST': 
+        # Get the desired book copy
+        id = request.POST['bid']
+        book = BookCopy.objects.filter(id = id, status = False).first()
+
+        # If not found, book = None
+        if book != None:
+            # Set status to available
+            book.status = True
+            # Set borrower to none
+            book.borrower = None
+            # Set borrow date to none
+            book.borrow_date = None
+            # Save the object
+            book.save()
+            msg = 'success'
+        else:
+            msg = 'failure'
+
+        response_data = {
+            'message': msg,
+        }
+        return JsonResponse(response_data)
+
+    # This page is not available for user to view and is just for data exchange
     else:
-        msg = 'failure'
-    response_data = {
-        'message': msg,
-    }
-    return JsonResponse(response_data)
+        raise Http404
 
 
 @csrf_exempt
 @login_required
 def ChangeRatingView(request):
-    if request.method == 'POST':    
+    if request.method == 'POST':
+        # Check if the user has already rated the same book
         book_id = request.POST['bid']
         value = request.POST['value']
         book = UserRating.objects.filter(book_id = book_id, user = request.user)
+        # Book for which the rating is given
+        b = Book.objects.filter(id = book_id)[0]
+
+        # If found, change the rating
         if len(book) != 0:
             book = book.first()
             book.rating = value
             book.save()
+        # Else, create the new UserRating object
         else:
-            b = Book.objects.filter(id = book_id)[0]
+            # New Rating object
             bnb = UserRating(book = b, user = request.user, rating = value)
+            # Save the rating
             bnb.save()
 
+        ## WHEN USER HAS CHANGED RATING, AVG RATING OF THE BOOK CHANGES ##
 
-        avgratings = UserRating.objects.filter(book_id = book_id)
+        # All rating objects for the book
+        totalRatings = UserRating.objects.filter(book_id = book_id)
+        # Avg rating variable
         r = 0
-        if len(avgratings) != 0:
-            for i in avgratings:
-                r += i.rating
-            r /= len(avgratings)
-            b = Book.objects.filter(id = book_id)[0]
-            b.rating = round(r, 2)
-            b.save()
+        # If rating objects are found for the book
+        if len(totalRatings) != 0:
+            for Rating in totalRatings:
+                r += Rating.rating
+            r /= len(totalRatings)
+        # If no user has rated the book
         else:
-            b.rating = -1
-            b.save()
+            # -1 indicates no rating is available
+            r = -1
+        # Save this rating to two decimal points
+        b.rating = round(r, 1)
+        b.save()
+        
         context = {
-            'rating': round(r,2),
+            'rating': round(r,1),
         }
         return JsonResponse(context)
+
+    # This page is not available for user to view and is just for data exchange
     else:
         raise Http404
 
 @csrf_exempt
 @login_required
 def RemoveRatingView(request):
-    #raise Http404('sahi tha')
     if request.method == 'POST':
+        # Check if the user has already rated the same book
         book_id = request.POST['bid']
-        book = UserRating.objects.filter(book_id = book_id, user = request.user)
-        if len(book) != 0:
-            book = book.first()
-            book.delete()
-            msg = 'success'
-            book = UserRating.objects.filter(book_id = book_id)
-            if len(book) != 0:
-                r = 0
-                for i in book:
-                    r += i.rating
-                r /= len(book)
-                val = r
-
-            else:
-                val = -1
-
-        else:
-            # no rating found
-            msg = 'fail'
+        rating = UserRating.objects.filter(book_id = book_id, user = request.user)
         b = Book.objects.filter(id = book_id).first()
-        b.rating = val
-        b.save()
-        print('\n\n\n\n\n', msg ,val)
+
+        # If user has rated, delete the rating object
+        # And update average rating of the book
+        if len(rating) != 0:
+            rating = rating.first()
+            rating.delete()
+            msg = 'success'
+
+            ## AVERAGE RATING ##
+
+            # All rating objects associated with the book
+            rating = UserRating.objects.filter(book_id = book_id)
+            if len(rating) != 0:
+                # variable to store the average rating
+                r = 0
+                # Take average of each rating
+                for i in rating:
+                    r += i.rating
+                r /= len(rating)
+
+            # No rating object is found
+            else:
+                r = -1
+
+            # Update the rating
+            b.rating = r
+            b.save()
+
+        # If rating wasn't found, send fail msg to the browser
+        else:
+            msg = 'fail'
+            # If no change was down, r = -2
+            r = -2
+
+
         context = {
             'msg': msg,
-            'value': val
+            'value': r
         }
         return JsonResponse(context)
+
+    # This page is not available for user to view and is just for data exchange
     else:
         raise Http404
